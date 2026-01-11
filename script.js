51122079e031859c4b3c1c3185b82c52759c0bd6
sarahroon <sarahrooney130500@gmail.com>
2026-01-11 19:37:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,3 @@ document.documentElement.scrollTop > 20) {
   } else {
     mybutton.style.display = "none";
   }
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-let counter = 1;
-
-  function scrolltothetop() {
-    const content = document.getElementById("content");
-    const scrollBox = document.getElementById("scrollBox");
-
-    const msg = document.createElement("div");
-    msg.textContent = "Message " + counter++;
-    content.appendChild(msg);
-
-    scrollBox.scrollTop = 0;
-  }
