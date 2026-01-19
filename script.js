d87d34f5ae6676affa9ff53db88e74cbd711246b
sarahroon <sarahrooney130500@gmail.com>
2026-01-19 18:01:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 20);
 document.documentElement.scrollTop > 20);
     mybutton.style.display = "block";
-  } else {
+}
+  else {
     mybutton.style.display = "none";
-  }
 }
